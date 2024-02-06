ecc320b305a63555a97654e0a1fcf9da49141d34
aleksanderlukianczuk03 <aleksander.lukianczuk@gmail.com>
2024-02-06 22:25:48 +0100

    finished project

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import {client} from "../../lib/sanity";
+import {fullBlog} from "../../lib/interface";
+import {urlFor} from "../../lib/sanity";
+import { PortableText } from "@portabletext/react";
+
+async function getData(slug: string) {
+    const query = `
+    *[_type == 'blog' && slug.current == '${slug}'] {
+        "currentSlug": slug.current,
+          title,
+          content,
+          titleimage
+      } [0]`;
+    const data = await client.fetch(query);
+    return data;
+}
+
+export default async function BlogArticle({params}: {params: {slug: string}}) {
+    const data: fullBlog = await getData(params.slug);
+  
+    return (
+        <div className="mt-8">
+            <h1>
+                <span className="block text-base text-center text-primary font-semibold tracking-wide uppercase"> 
+                Mój Blog Alex
+                </span>
+                <span className="mt-2 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl">
+                    {data.title}
+                     </span>
+            </h1>
+            <Image
+                src={urlFor(data.titleimage).url()}
+                alt="Title Image"
+                width={500}
+                height={500}
+                priority
+                className="rounded-t-lg mt-8 mx-auto border"
+            />
+
+            <div className="mt-16 prose prose-blue prose-lg dark:prose-invert">
+
+        <PortableText value={data.content} />
+            </div>
+        </div>
+    );
+}
---
+++ app/lib/interface.ts
@@ -0,0 +1,13 @@
+export interface simpleBlogCard {
+  title: string;
+  smallDescription: string;
+  currentSlug: string;
+  titleimage: any;
+}
+
+export interface fullBlog {
+    currentSlug: string;
+    title: string;
+    content: any;
+    titleimage: any;
+}
