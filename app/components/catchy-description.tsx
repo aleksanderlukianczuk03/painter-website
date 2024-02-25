4bb1c6744e3ac74802b81b8f55c23395bb7c18e6
aleksanderlukianczuk03 <aleksander.lukianczuk@gmail.com>
2024-02-25 21:16:44 +0100

    update

--- app/components/catchy-description.tsx
+++ app/components/catchy-description.tsx
@@ -5,17 +5,27 @@ function CatchyText() {
         
         <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', maxWidth: '740px', margin: '30px auto' }}>
         <div>
-            <h1 style={{ fontSize: 'clamp(68px, 8vw, 52px)', fontFamily: 'Inter, sans-serif', fontWeight: 800, textAlign: 'center', overflow: 'hidden', textOverflow: 'ellipsis', lineHeight: '1.2em', whiteSpace: 'pre-wrap', margin: '10px 10px' }}>
-            Science with a
-                <br />
-                <span style={{ color: '#42bd41' }}>smile</span>
-            </h1>
-        </div>
-        <div style={{ marginTop: '10px', marginBottom: '30px' }}>
-    <p style={{ fontSize: 'clamp(24px, 5vw, 20px)', fontFamily: 'Inter, sans-serif', fontWeight: 500, textAlign: 'center', overflow: 'hidden', textOverflow: 'ellipsis', lineHeight: '1.2em', whiteSpace: 'pre-wrap', margin: '10px 10px', color: '#333' }}>
-        At Immunifai, humor lightens the 
+    <h1 style={{ 
+        fontSize: 'clamp(30px, 5vw, 68px)', 
+        fontFamily: 'Inter, sans-serif', 
+        fontWeight: 800, 
+        textAlign: 'center', 
+        overflow: 'hidden', 
+        textOverflow: 'ellipsis', 
+        lineHeight: '1.2em', 
+        whiteSpace: 'pre-wrap', 
+        margin: '10px 10px' 
+    }}>
+        Be Smart, Choose
         <br />
-        science of health.
+        <span style={{ color: '#42bd41' }}>Science</span>
+    </h1>
+</div>
+        <div style={{ marginTop: '10px', marginBottom: '30px', margin: '0' }}>
+    <p style={{ fontSize: 'clamp(24px, 5vw, 20px)', fontFamily: 'Inter, sans-serif', fontWeight: 500, textAlign: 'center', overflow: 'hidden', textOverflow: 'ellipsis', lineHeight: '1.2em', whiteSpace: 'pre-wrap', margin: '10px 10px', color: '#333' }}>
+    At Immunifai, everything is
+        <br />
+        about science.
         <br />
     </p>
 </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "./components/theme-provider";
+// import { ThemeProvider } from "./components/theme-provider";
 import Navbar from "./components/navbar";
 
 
@@ -34,16 +34,11 @@ export default function RootLayout({
   return (
     <html lang="en" >
       <body className={inter.className} >
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
+        
             <Navbar />
           
 <main className="max-w-full mx-auto px-6 sm:px-20 lg:px-40">{children}</main> 
-        </ThemeProvider>
+        
         </body>
     </html>
   );
